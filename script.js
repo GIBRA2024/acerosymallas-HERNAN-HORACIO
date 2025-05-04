659a472bc753a0322b0b6238b8ec01e096202ebf
Mateo Monroy <monroymateo1@gmail.com>
2025-05-04 12:50:10 -0500

    listo

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ N:Arango Ramirez;Hernan;Horacio;;
 FN:Hernan Horacio Arango Ramirez
 TEL:+57 3207869989
 EMAIL;TYPE=WORK: produccion@acerosymallas.com
-NOTE: Bon Vivant
+NOTE: Representante Legal
 END:VCARD`;
 
     const blob = new Blob([vcardData], { type: 'text/vcard' });
