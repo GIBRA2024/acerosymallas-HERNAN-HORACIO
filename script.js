2dc20433a25623ccaa0aea41ee23237a0cafa348
GIBRA2024 <gibra.company@gmail.com>
2025-06-18 11:05:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ N:Arango Ramirez;Hernan;Horacio;;
 FN:Hernan Horacio Arango Ramirez
 TEL:+57 3207869989
 EMAIL;TYPE=WORK: produccion@acerosymallas.com
-NOTE: Representante Legal
+NOTE: Director de Producción
 END:VCARD`;
 
     const blob = new Blob([vcardData], { type: 'text/vcard' });
